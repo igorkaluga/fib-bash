a08171961cb55a1946b3435af4f42ffcfe3a2dad
Igor Orlov <63825390+igorkaluga@users.noreply.github.com>
2022-10-13 13:49:23 -0700

    Create fibs.sh

---
+++ fibs.sh
@@ -0,0 +1,27 @@
+#####################
+function fib () {
+        fib=$1
+        cur=1
+        prev=0
+        cnt=1
+        while [ "$cnt" -lt "$fib" ]; do
+                tmp_cur=$cur
+                cur=$(($cur + $prev))
+                prev=$tmp_cur
+                cnt=$(($cnt + 1))    
+        done
+        echo "fib of $fib is $cur"
+}
+fib 8
+function rec_fib () {
+        if [ "$1" = "0" ] || [ "$1" = "1" ]; then
+                return $1
+        else    
+                #rec_fib($1 - 1) + rec_fib($ - 2)
+                return $(( $(rec_fib $(($1 - 1)) + $(rec_fib $(($1 - 2))))))    
+        fi
+}
+rec_fib 1
+#test_var=rec_fib 1
+#echo $test_var
+
